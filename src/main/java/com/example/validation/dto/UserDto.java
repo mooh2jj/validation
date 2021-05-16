7bb54792bfb26a98a7df60da28a469a0a8322eb7
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-05-16 20:06:22 +0900

    @AssertTrue boolean 검증 구현

--- src/main/java/com/example/validation/dto/UserDto.java
+++ src/main/java/com/example/validation/dto/UserDto.java
@@ -3,6 +3,8 @@ package com.example.validation.dto;
 import lombok.Data;
 
 import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Data
 public class UserDto {
@@ -18,4 +20,20 @@ public class UserDto {
 
     @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다.")
     private String phoneNumber;
+
+    @Size(min=6, max=6)
+    private String reqYearMonth;    // yyyymm
+
+    @AssertTrue(message = "yyyyMM의 형식에 맞지 않습니다.")
+    public boolean isreqYearMonthValidation(){
+//        this.reqYearMonth = getReqYearMonth() + "01";
+        try {
+            LocalDate localDate = LocalDate.parse(getReqYearMonth()+ "01", DateTimeFormatter.ofPattern("yyyyMMdds"));
+
+        }catch (Exception e){
+            return false;
+        }
+
+        return true;
+    }
 }
